dd6cb0bff0f5995f09622caf4fec62544ca351d4
YahavBenHur <Yahavbenhur@gmail.com>
2025-11-19 18:46:42 +0200

    Updated Yahav.java print message

--- Yahav.java
+++ Yahav.java
@@ -1,7 +1,7 @@
 public class Yahav {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Yahav");
+            System.out.println("Yahav2");
         }
     }
 }
